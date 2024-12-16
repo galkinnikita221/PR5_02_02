6be88807fc8902b67d64a143a2200f0aef6b0a8c
Nikita <galkinnikita228@gmail.com>
2024-12-16 19:48:22 +0500

    Реализованы команды подключения и помощи. Galkin

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,15 +1,174 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Sockets;
+using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Client
 {
-    internal class Program
+    public class Program
     {
+        static IPAddress ServerIpAddress;
+        static int ServerPort;
+        static string ClientToken;
+        static DateTime ClientDateConnection;
+
         static void Main(string[] args)
         {
+            
         }
+
+        static void SetCommand()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            string Command = Console.ReadLine();
+
+            if (Command == "/config")
+            {
+                File.Delete(Directory.GetCurrentDirectory() + "/.config");
+                OnSettings();
+            }
+            else if (Command == "/connect") ConnectServer();
+            else if (Command.Contains("/connect with login")) ConnectServerWithToken(Command);
+            else if (Command == "/status") GetStatus();
+            else if (Command == "/help") Help();
+        }
+
+        static void Help()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Commands to the clients: ");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("/config");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(" - set initial settings");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("/connect");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(" - connection to the server");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("/status");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(" - show list users");
+        }
+
+        static void ConnectServer()
+        {
+            IPEndPoint EndPoint = new IPEndPoint(ServerIpAddress, ServerPort);
+            Socket Socket = new Socket(
+                    AddressFamily.InterNetwork,
+                    SocketType.Stream,
+                    ProtocolType.Tcp);
+
+            try
+            {
+                Socket.Connect(EndPoint);
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            if (Socket.Connected)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Connection to server successful");
+
+                Socket.Send(Encoding.UTF8.GetBytes("/token"));
+
+                byte[] Bytes = new byte[10485760];
+                int ByteRec = Socket.Receive(Bytes);
+
+                string Responce = Encoding.UTF8.GetString(Bytes, 0, ByteRec);
+
+                if (Responce == "/limit")
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("There is not enough space on the license server");
+                }
+                else if (Responce == "/blacklist")
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("This client in blacklist");
+                }
+                else if (Responce == "/fake")
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("This client isn't in database");
+                }
+                else
+                {
+                    ClientToken = Responce;
+                    ClientDateConnection = DateTime.Now;
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Received connection token: " + ClientToken);
+                }
+            }
+        }
+        static void ConnectServerWithToken(string command)
+        {
+            IPEndPoint EndPoint = new IPEndPoint(ServerIpAddress, ServerPort);
+            Socket Socket = new Socket(
+                    AddressFamily.InterNetwork,
+                    SocketType.Stream,
+                    ProtocolType.Tcp);
+
+            try
+            {
+                Socket.Connect(EndPoint);
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            if (Socket.Connected)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Connection to server successful");
+
+                string Token = command.Replace("/connect with login ", "");
+
+                Socket.Send(Encoding.UTF8.GetBytes("/with login " + Token));
+
+                byte[] Bytes = new byte[10485760];
+                int ByteRec = Socket.Receive(Bytes);
+
+                string Responce = Encoding.UTF8.GetString(Bytes, 0, ByteRec);
+
+                if (Responce == "/limit")
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("There is not enough space on the license server");
+                }
+                else if (Responce == "/blacklist")
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("This client in blacklist");
+                }
+                else if (Responce == "/fake")
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("This client isn't in database");
+                }
+                else
+                {
+                    ClientToken = Responce;
+                    ClientDateConnection = DateTime.Now;
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Received connection token: " + ClientToken);
+                }
+            }
+        }
+
     }
 }
