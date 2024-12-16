8f2d9b306779b97450d46a1045089439809e35ad
Nikita <galkinnikita228@gmail.com>
2024-12-16 18:32:13 +0500

    Реализованы классы для подключения к базе данных сервер. Galkin

---
+++ Server/Classes/Client.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Classes
+{
+    public class Client
+    {
+        public string Login { get; set; }
+        public string Token { get; set; }
+        public DateTime DateConnect { get; set; }
+        public Client()
+        {
+            Random random = new Random();
+            string Chars = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm0123456789";
+
+            Token = new string(Enumerable.Repeat(Chars, 15).Select(x => x[random.Next(Chars.Length)]).ToArray());
+            DateConnect = DateTime.Now;
+        }
+    }
+}
---
+++ Server/Classes/Connection.cs
@@ -0,0 +1,30 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Classes
+{
+    public class Connection
+    {
+        private static string conf = "server=localhost;port=3306;database=pr5;uid=root;pwd=root;";
+        public static MySqlConnection OpenConnection()
+        {
+            MySqlConnection connection = new MySqlConnection(conf);
+            connection.Open();
+            return connection;
+        }
+        public static MySqlDataReader Query(string Query, MySqlConnection connection)
+        {
+            MySqlCommand command = new MySqlCommand(Query, connection);
+            return command.ExecuteReader();
+        }
+        public static void CloseConnection(MySqlConnection connection)
+        {
+            connection.Close();
+            MySqlConnection.ClearPool(connection);
+        }
+    }
+}
